986e9fdf057f931e677af5568d6c0d7dcfa20ea7
Dom Twitty <dktwitty@gmail.com>
2024-09-27 16:45:57 -0700

    Almost done with RIFL

--- src/charm/state_machine.rs
+++ src/charm/state_machine.rs
@@ -1,15 +1,17 @@
 use crate::raft;
 use crate::raft::types::RaftInfo;
 use crate::tracing_util::SpanOption;
+use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use tokio::sync::oneshot;
 use tonic::async_trait;
 use tracing::Span;
+use uuid::Uuid;
 
 struct History {
     current: Option<String>,
-    events: Vec<Event>,
+    events: IndexMap<RequestId, Event>,
 }
 
 pub struct CharmStateMachine {
@@ -50,11 +52,13 @@ pub struct DeleteResponse {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum Event {
     Put {
+        request_id: RequestId,
         value: String,
         raft_info: RaftInfo,
     },
 
     Delete {
+        request_id: RequestId,
         raft_info: RaftInfo,
     },
 }
@@ -63,6 +67,12 @@ pub enum Event {
 pub struct HistoryResponse {
     pub events: Vec<Event>,
     pub raft_info: RaftInfo,
+}
+
+#[derive(Debug, Serialize, Deserialize, Hash, Clone, PartialEq, Eq, Ord, PartialOrd)]
+pub struct RequestId {
+    pub client_id: Uuid,
+    pub request_number: u64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -75,6 +85,7 @@ pub enum CharmStateMachineRequest {
         span: Option<Span>
     },
     Set {
+        request_id: RequestId,
         key: String,
         value: String,
         #[serde(skip)]
@@ -83,6 +94,7 @@ pub enum CharmStateMachineRequest {
         span: Option<Span>
     },
     Delete {
+        request_id: RequestId,
         key: String,
         #[serde(skip)]
         response: Option<oneshot::Sender<DeleteResponse>>,
@@ -116,14 +128,26 @@ impl raft::state_machine::StateMachine for CharmStateMachine {
                     }
                 }
             }
-            CharmStateMachineRequest::Set { key, value, response, span } => {
+            CharmStateMachineRequest::Set { request_id, key, value, response, span } => {
                 span.in_scope(|| {
                     tracing::debug!("Set `{:?}` to `{:?}`", key, value);
                 });
-                let history = self.data.entry(key.clone()).or_insert_with(|| History { current: None, events: Vec::new() });
-                history.current = Some(value.clone());
-                history.events.push(Event::Put { value, raft_info: raft_info.clone() });
+                let history = self.data.entry(key.clone()).or_insert_with(|| History { current: None, events: IndexMap::new() });
+
+                if let Some(Event::Put { value: old_value, raft_info, .. }) = history.events.get(&request_id) {
+                    if let Some(tx) = response {
+                        tracing::debug!("Replaying request `{:?}`", request_id);
+                        let r = tx.send(SetResponse { raft_info: raft_info.clone() });
+                        if r.is_err() {
+                            tracing::warn!("Failed to send response from Set request. The receiver has been dropped.");
+                        }
+                    }
+                    return;
+                }
                 
+                history.current = Some(value.clone());
+                history.events.insert(request_id.clone(), Event::Put { request_id, value, raft_info: raft_info.clone() });
+
                 if let Some(tx) = response {
                     let r = tx.send(SetResponse { raft_info });
                     if r.is_err() {
@@ -131,13 +155,25 @@ impl raft::state_machine::StateMachine for CharmStateMachine {
                     }
                 }
             }
-            CharmStateMachineRequest::Delete { key, response, span } => {
+            CharmStateMachineRequest::Delete { request_id, key, response, span } => {
                 span.in_scope(|| {
                     tracing::debug!("Delete `{:?}`", key);
                 });
-                let history = self.data.entry(key.clone()).or_insert_with(|| History { current: None, events: Vec::new() });
+                let history = self.data.entry(key.clone()).or_insert_with(|| History { current: None, events: IndexMap::new() });
+
+                if let Some(Event::Delete { raft_info, .. }) = history.events.get(&request_id) {
+                    if let Some(tx) = response {
+                        tracing::debug!("Replaying request `{:?}`", request_id);
+                        let r = tx.send(DeleteResponse { raft_info: raft_info.clone() });
+                        if r.is_err() {
+                            tracing::warn!("Failed to send response from Delete request. The receiver has been dropped.");
+                        }
+                    }
+                    return;
+                }
+                
                 history.current = None;
-                history.events.push(Event::Delete { raft_info: raft_info.clone() });
+                history.events.insert(request_id.clone(), Event::Delete { request_id, raft_info: raft_info.clone() });
                 if let Some(tx) = response {
                     let r = tx.send(DeleteResponse { raft_info });
                     if r.is_err() {
@@ -151,7 +187,7 @@ impl raft::state_machine::StateMachine for CharmStateMachine {
                 response,
                 span,
             } => {
-                let events = self.data.get(&key).map(|history| history.events.clone()).unwrap_or_default();
+                let events = self.data.get(&key).map(|history| history.events.values().collect()).unwrap_or_default();
                 span.in_scope(|| {
                     tracing::debug!("History for `{:?}`: `{:?}`", key, events);
                 });
